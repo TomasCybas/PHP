47d79d82df3248342df27f3a6ebf242b1f8dd48c
Tomas <tcybas@gmail.com>
2019-09-19 22:33:25 +0300

    Auto stash before merge of "master" and "origin/master"

--- PHP_kursas/draudimas/_ide_helper.php
+++ PHP_kursas/draudimas/_ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.0.3 on 2019-09-19 10:57:01.
+ * Generated for Laravel 6.0.3 on 2019-09-19 19:25:37.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
