8aa95b567f4bdcee4748218affb8c04b0620f45a
Tomas <tcybas@gmail.com>
2019-09-20 12:23:08 +0300

    Implemented car edit and delete functionality

--- PHP_kursas/draudimas/routes/web.php
+++ PHP_kursas/draudimas/routes/web.php
@@ -25,9 +25,12 @@ Route::post('/owners/store', 'OwnerController@storeOwner')->name('owners.store')
 Route::get('owners/details/{id}', 'OwnerController@showOwner')->name('owners.details');
 Route::get('owners/edit/{id}', 'OwnerController@editOwner')->name('owners.edit');
 Route::post('owners.details/{id}', 'OwnerController@updateOwner')->name('owners.update');
-Route::get('owners.delete{id}', 'OwnerController@deleteOwner')->name('owners.delete');
+Route::get('owners.delete/{id}', 'OwnerController@deleteOwner')->name('owners.delete');
 
 
 Route::get('/cars', 'CarController@index')->name('cars');
-Route::get('/cars/add{owner_id}', 'CarController@addCar')->name('cars.add');
-Route::post('/cars/store{owner_id}', 'CarController@storeCar')->name('cars.store');
+Route::get('/cars/add/{owner_id}', 'CarController@addCar')->name('cars.add');
+Route::post('/cars/store/{owner_id}', 'CarController@storeCar')->name('cars.store');
+Route::get('/cars/edit/{id}', 'CarController@editCar')->name('cars.edit');
+Route::post('/cars/update/{id}', 'CarController@updateCar')->name('cars.update');
+Route::get('/cars/delete/{id}{owner_id}', 'CarController@deleteCar')->name('cars.delete');
